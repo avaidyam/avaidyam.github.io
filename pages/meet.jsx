59173f72611026e8b05dc1cef079c380b458bdb0
Aditya Vaidyam <avaidyam@users.noreply.github.com>
2023-06-28 16:36:22 -0500

    Update meet.jsx

--- pages/meet.jsx
+++ pages/meet.jsx
@@ -10,7 +10,7 @@ export default function Meet() {
       <section style={{ display: "flex", alignItems: "center", padding: 0, width: "100%" }}>
         <iframe 
           src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ1llbLe11GwGN2S_HB2Y1Qv_LW5rgxBUdUAvdgCTPRpVD6xX8BnGkAAM4GE3bCLXKlst-5f4cGE?gv=true" 
-          style={{ border: "none", width: "100%", height: "100vh" }} />
+          style={{ border: "none", width: "100%", height: "92vh" }} />
       </section>
     </Layout>
   );
