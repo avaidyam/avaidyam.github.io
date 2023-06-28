1d730e2940db9bf7863ab382e65dc449132f6d59
Aditya Vaidyam <avaidyam@users.noreply.github.com>
2023-06-28 16:20:37 -0500

    Update meet.jsx

--- pages/meet.jsx
+++ pages/meet.jsx
@@ -7,10 +7,10 @@ import styles from './index.module.css';
 export default function Meet() {
   return (
     <Layout title="Aditya Vaidyam" description="Aditya Vaidyam">
-      <section style={{ display: "flex", alignItems: "center", padding: "2rem 0", width: "100%" }}>
+      <section style={{ display: "flex", alignItems: "center", padding: 0, width: "100%" }}>
         <iframe 
           src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ1llbLe11GwGN2S_HB2Y1Qv_LW5rgxBUdUAvdgCTPRpVD6xX8BnGkAAM4GE3bCLXKlst-5f4cGE?gv=true" 
-          style={{ border: "none", width: "100%", height: 600 }} />
+          style={{ border: "none", width: "100%", height: "100vh" }} />
       </section>
     </Layout>
   );
